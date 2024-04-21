8ba757d3cebe6ff056e53e480055d3506919fd90
lucasezidro <lucasezidro7@gmail.com>
2024-04-20 21:41:56 -0300

    create tokens - react folder and config stitches

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,38 @@
+export const colors = {
+  white: '#FFF',
+  black: '#000',
+
+  gray100: '#E1E1E6',
+  gray200: '#A9A9B2',
+  gray400: '#7C7C8A',
+  gray500: '#505059',
+  gray600: '#323238',
+  gray700: '#29292E',
+  gray800: '#202024',
+  gray900: '#121214',
+
+  green300: '#00B37E',
+  green500: '#00875F',
+  green700: '#015F43',
+  green900: '#00291D',
+
+  red300: '#f87171',
+  red500: '#dc2626',
+  red700: '#b91c1c',
+  red900: '#7f1d1d',
+
+  yellow300: '#fbbf24',
+  yellow500: '#f59e0b',
+  yellow700: '#b45309',
+  yellow900: '#451a03',
+
+  purple300: '#a78bfa',
+  purple500: '#a855f7',
+  purple700: '#7e22ce',
+  purple900: '#581c87',
+
+  rose300: '#fb7185',
+  rose500: '#f43f5e',
+  rose700: '#be123c',
+  rose900: '#881337',
+}
